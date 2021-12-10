14a16e5541166e27025f7ca4551923a4d7719e9a
LongTien <h2so4allii@gmail.com>
2021-12-10 14:26:16 +0700

    ss8

--- src/ss8_clean_code_va_refactoring/thuc_hanh/refactoring_doi_ten_va_tach_hang/Caculator.java
+++ src/ss8_clean_code_va_refactoring/thuc_hanh/refactoring_doi_ten_va_tach_hang/Caculator.java
@@ -1,37 +1,27 @@
 package ss8_clean_code_va_refactoring.thuc_hanh.refactoring_doi_ten_va_tach_hang;
 
 public class Caculator {
-    public static int Calculate(int firstOperand, int secondOperand, char opetator  ) {
+
+    public static final char ADDITION = '+';
+    public static final char SUBTRACTION = '-';
+    public static final char MULTIPLICATION = '*';
+    public static final char DIVISION = '/';
+
+    public static int calculate(int firstOperand, int secondOperand, char opetator) {
         switch (opetator) {
-            case '+':
-                return ADDTION(firstOperand, secondOperand);
-            case '-':
-                return SUBTRACTION(firstOperand, secondOperand);
-            case '*':
-                return MULTIPLICATION(firstOperand, secondOperand);
-            case '/':
+            case ADDITION:
+                return firstOperand + secondOperand;
+            case SUBTRACTION:
+                return firstOperand - secondOperand;
+            case MULTIPLICATION:
+                return firstOperand * secondOperand;
+            case DIVISION:
                 if (secondOperand != 0)
-                    return DIVISION(firstOperand, secondOperand);
+                    return firstOperand / secondOperand;
                 else
                     throw new RuntimeException("Can not divide by 0");
             default:
                 throw new RuntimeException("Unsupported operation");
         }
-    }
-
-    private static int MULTIPLICATION(int firstOperand, int secondOperand) {
-        return firstOperand * secondOperand;
-    }
-
-    private static int DIVISION(int firstOperand, int secondOperand) {
-        return firstOperand / secondOperand;
-    }
-
-    private static int SUBTRACTION(int firstOperand, int secondOperand) {
-        return firstOperand - secondOperand;
-    }
-
-    private static int ADDTION(int firstOperand, int secondOperand) {
-        return firstOperand + secondOperand;
     }
 }
